e4f1e496e95d764a965aabbed7334737ef7ecb0e
Pavel <pavel.filkovskiy@gmail.com>
2019-08-21 21:40:28 +0100

    Update displayNumber.c

--- displayNumber.c
+++ displayNumber.c
@@ -1,13 +1,15 @@
 #define F_CPU 8000000  // Hz
 #include <avr/io.h>
-#include <math.h> // usage of pow() function
 #include <util/delay.h>
 #include <stdlib.h>
+#include <math.h> // contains pow() aka power x^y
 
 // #define nop() {asm("nop");}
 
+// parameter for conversion a number into an array, sets length of the array.
 const uint8_t precision = 3;
 
+// byte-codes for a digits to be displayed at the connected to the according PORT of the microcontroller.
 const uint8_t digits[] = {
 	0b00111111, // 0
 	0b00000110, // 1
@@ -21,6 +23,9 @@ const uint8_t digits[] = {
 	0b01101111  // 9
 };
 
+/*
+	returns lengths of integer number in digits. e.g: length(999) --> 3
+ */
 const uint8_t length(uint8_t number) {
 	uint8_t counter = 0;
 	if (number == 0) {
@@ -34,8 +39,15 @@ const uint8_t length(uint8_t number) {
 	return counter;
 }
 
-void numberToArray(uint8_t number, uint8_t* n) {
-
+/* 
+	Converts multiple-digit integer number into an array of integers sized as precision.
+	If length of number less than precision, then adds leading zeros.
+	e.g: precision = 3, then number = 99 --> array: [0, 9, 9]
+ */
+uint8_t* numberToArray(uint8_t number) {
+	
+	uint8_t* n = (uint8_t*)malloc(precision * sizeof(uint8_t));
+	
 	if (length(number) < precision) {
 		for (uint8_t i = 0; i < (precision - length(number)); i++) {
 			n[i] = 0;
@@ -50,14 +62,18 @@ void numberToArray(uint8_t number, uint8_t* n) {
 		mod = nm % (uint8_t)pow(10, length(nm) - 1);
 		n[precision - length(number) + i] = (nm - mod) / denumerator;
 	}
-
+	
+	return n;
 }
 
+/* 
+	Displays multiple-digit (integer) number represented as an array. e.g.: 1234 --> [1,2,3,4]
+	If length of number less than precision, then adds leading zeros.
+ */
 void displayNumber(uint8_t number) {
 	
-	uint8_t* n = (uint8_t*)malloc(precision * sizeof(uint8_t));
+	uint8_t* n = numberToArray(number);
 	
-	numberToArray(number, n);
 	for (uint8_t i = 0; i < precision; ++i) {
 		PORTD = ~(digits[n[i]]);
 		_delay_ms(250);
@@ -72,7 +88,10 @@ void displayNumber(uint8_t number) {
 	// this requires #include <stdlib.h>
 }
 
-void display1DNumber(uint8_t number) {
+/* 
+	Displays single-digit numbers [0-9]:
+ */
+void displayOneDigitDNumber(uint8_t number) {
 	PORTD = ~(digits[number]);
 	_delay_ms(250);
 	PORTD = ~(0);
@@ -85,7 +104,7 @@ int main(void) {
 	
 	while (1) {
 		for (int i = 0; i<20; i++) {
-			// display1DNumber(i);
+			// displayOneDigitDNumber(i);
 			displayNumber(i);
 		}
 	}
